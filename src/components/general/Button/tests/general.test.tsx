36d4dc6480cac879fd3af6095e71841938462917
Rafael Corrêa Chaves <rafael.chaves98@hotmail.com>
2022-01-19 16:12:26 -0300

    [setup] structure (#8)
    * added typescript
    
    * moved pages into src scope
    
    * moved styles into src scope
    
    * added storybook
    
    * updated files to typescript
    
    * updated readme
    
    * added security markdown
    
    * added formalization for code
    
    * added testing library
    
    * added jest configuration
    
    * updated scripts
    
    * updated workflow
    
    * remove audit because of storybook
    
    * removed unused references
    
    * added mock test

---
+++ src/components/general/Button/tests/general.test.tsx
@@ -0,0 +1,19 @@
+// Packages
+import { fireEvent, render, screen } from "@testing-library/react"
+
+// Component
+import Button from ".."
+
+describe("Button general tests", () => {
+	test("Test if component breaks on render", () => {
+		// Arrange
+		const callback = jest.fn()
+
+		// Act
+		render(<Button onClick={callback}>Test button</Button>)
+		fireEvent.click(screen.getByText("Test button"))
+
+		// Assert
+		expect(callback).toBeCalled()
+	})
+})
