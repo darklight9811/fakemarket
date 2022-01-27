bc6ef8e1713c71a0bf82b99b4e82e451f3c4ea79
Rafael Correa <rafael.chaves98@hotmail.com>
2022-01-27 02:33:56 -0300

    fixed price generation

--- src/store/models/assets/utils.ts
+++ src/store/models/assets/utils.ts
@@ -4,7 +4,7 @@ import { add, autoPipe, keep, rename } from "../../../utils/functional"
 const sanitizer = autoPipe(
 	keep("asset_id", "data", "name", "schema"),
 	rename({asset_id: "id"}),
-	add(() => ({ price: `${(Math.random() * 0.1).toFixed(3)} WAX` })),
+	add(() => ({ price: `${(Math.random() * 0.1 + 0.001).toFixed(3)} WAX` })),
 )
 
 export const sanitize = (asset: AssetType | AssetType[]) => sanitizer(asset)
